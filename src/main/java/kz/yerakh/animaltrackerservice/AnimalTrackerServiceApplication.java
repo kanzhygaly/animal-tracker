07c537fe6d5b035287351be77cb79e1c4d8f722c
Yerlan Akhmetov <kanzhygaly@gmail.com>
2023-02-06 22:28:26 +0200

    init spring boot

---
+++ src/main/java/kz/yerakh/animaltrackerservice/AnimalTrackerServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.yerakh.animaltrackerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalTrackerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalTrackerServiceApplication.class, args);
+	}
+
+}
