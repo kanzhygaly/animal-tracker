a4f6bd3c7067f9935fb81ed611daefdeff1a1665
Yerlan Akhmetov <kanzhygaly@gmail.com>
2023-03-05 20:46:47 +0200

    AccountRepositoryIntegrationTest

--- src/test/java/kz/yerakh/animaltrackerservice/repository/AccountRepositoryIntegrationTest.java
+++ src/test/java/kz/yerakh/animaltrackerservice/repository/AccountRepositoryIntegrationTest.java
@@ -10,29 +10,26 @@ import org.springframework.test.context.jdbc.Sql;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@Sql(value = "/db/populate_account.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
-@Sql(value = "/db/clean_account.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+//@Sql(value = "/db/clean_account.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 class AccountRepositoryIntegrationTest {
 
     @Autowired
     private AccountRepository testObj;
 
     @Test
-    void save_success() {
-        int rows = testObj.save(new AccountRequest("Victor", "Hugo", "victor.hugo@gmail.com", "DummyPassword"));
+    void save_find_and_update_success() {
+        var email = "victor.hugo@gmail.com";
+        int rows = testObj.save(new AccountRequest("Victor", "Hugo", email, "DummyPassword"));
         assertThat(rows).isEqualTo(1);
-    }
 
-    @Test
-    void find_and_update_success() {
-        var account = testObj.findByEmail("john.smith@gmail.com");
+        var account = testObj.findByEmail(email);
         assertThat(account).isPresent();
         var accountId = account.get().accountId();
 
-        var newEmail = "smith.john@gmail.com";
+        var newEmail = "hugo.victor@gmail.com";
         var updated = new AccountRequest(account.get().firstName(), account.get().lastName(), newEmail, "newPassword");
 
-        int rows = testObj.update(accountId, updated);
+        rows = testObj.update(accountId, updated);
         assertThat(rows).isEqualTo(1);
 
         account = testObj.findById(accountId);
@@ -41,13 +38,57 @@ class AccountRepositoryIntegrationTest {
     }
 
     @Test
-    void findByParams_and_delete_success() {
-        var searchCriteria = AccountSearchCriteria.builder().email("john.smith@gmail.com").from(0).size(1).build();
+    @Sql(value = "/db/populate_account.sql")
+    void findByParams_byEmail_and_delete_success() {
+        var searchCriteria = AccountSearchCriteria.builder().email("john.smith@gmail.com")
+                .from(0).size(10).build();
 
         var result = testObj.findByParams(searchCriteria);
-        assertThat(result).isNotEmpty();
+        assertThat(result).hasSize(1);
 
         int rows = testObj.delete(result.get(0).accountId());
         assertThat(rows).isEqualTo(1);
+    }
+
+    @Test
+    @Sql(value = "/db/populate_account.sql")
+    void findByParams_byFirstNameAndLastNameInLowerCase_success() {
+        var searchCriteria = AccountSearchCriteria.builder().firstName("john").lastName("smith")
+                .from(0).size(10).build();
+
+        var result = testObj.findByParams(searchCriteria);
+        assertThat(result).hasSize(2);
+    }
+
+    @Test
+    @Sql(value = "/db/populate_account.sql")
+    void findByParams_byLastNameInLowerCaseAndFirstNameAndEmailAreEmpty_success() {
+        var searchCriteria = AccountSearchCriteria.builder().firstName("").lastName("smith").email("")
+                .from(0).size(10).build();
+
+        var result = testObj.findByParams(searchCriteria);
+        assertThat(result).hasSize(3);
+    }
+
+    @Test
+    @Sql(value = "/db/populate_account.sql")
+    void findByParams_byFirstNameAndEmailInLowerCase_success() {
+        var searchCriteria = AccountSearchCriteria.builder().firstName("abraham").lastName("").email("alincoln@gmail.com")
+                .from(0).size(10).build();
+
+        var result = testObj.findByParams(searchCriteria);
+        assertThat(result).hasSize(1);
+    }
+
+    @Test
+    @Sql(value = "/db/populate_account.sql")
+    void findByParams_emptyParams_success() {
+        var searchCriteria = AccountSearchCriteria.builder()
+                .from(0).size(2).build();
+
+        var result = testObj.findByParams(searchCriteria);
+        assertThat(result).hasSize(2);
+        assertThat(result.get(0).accountId()).isEqualTo(1);
+        assertThat(result.get(1).accountId()).isEqualTo(2);
     }
 }
--- src/test/resources/db/populate_account.sql
+++ src/test/resources/db/populate_account.sql
@@ -1,2 +1,8 @@
 INSERT INTO account(first_name, last_name, email, password)
 VALUES('John', 'Smith', 'john.smith@gmail.com', 'DummyPassword') ON CONFLICT (email) DO NOTHING;
+INSERT INTO account(first_name, last_name, email, password)
+VALUES('Johnson', 'Smith', 'johnyboy@gmail.com', 'DummyPassword') ON CONFLICT (email) DO NOTHING;
+INSERT INTO account(first_name, last_name, email, password)
+VALUES('John', 'Smith', 'john_smith@gmail.com', 'DummyPassword') ON CONFLICT (email) DO NOTHING;
+INSERT INTO account(first_name, last_name, email, password)
+VALUES('Abraham', 'Lincoln', 'alincoln@gmail.com', 'DummyPassword') ON CONFLICT (email) DO NOTHING;
